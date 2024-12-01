05278d25bac5b3a8ec9fdd54c418236e86406f7d
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-12-01 14:35:14 +0200

    refactor/list-to-dictionary

--- main.py
+++ main.py
@@ -3,27 +3,40 @@ from question import Question
 import requests
 import time
 import html
+import json
 
 
 GRID_PAD = 20
 
 categories = [18, 9, 15, 31, 32]
 
-questions = []
-answers = []
+categories = {
+    "Video Games": 15,
+    "Computers": 18,
+    "General Knowledge": 9,
+    "Anime & Manga": 31,
+    "Cartoons & Animations": 32,
+}
 
-for i in range(5):
-    cat_request = requests.get(
-        f"https://opentdb.com/api.php?amount=5&category={categories[i]}"
-    )
+#TODO: Refactor this shit into functions and improve variable names
+questions = {}
+
+for category, value in categories.items():
+    cat_request = requests.get(f"https://opentdb.com/api.php?amount=5&category={value}")
     cat_request.raise_for_status()
-    print(f"Attempting request {i+1}, status: {cat_request}")
+    print(f"Attempting request {category}, status: {cat_request}")
     cat_results = cat_request.json()["results"]
-    cat_questions = [html.unescape(result["question"]) for result in cat_results]
-    cat_answers = [html.unescape(result["correct_answer"]) for result in cat_results]
-    questions.append(cat_questions)
-    answers.append(cat_answers)
+    questions[category] = []
+    for result in cat_results:
+        questions[category].append(
+            {
+                "question": html.unescape(result["question"]),
+                "answer": html.unescape(result["correct_answer"]),
+            }
+        )
     time.sleep(5)
+
+print(json.dumps(questions, indent=4))
 
 
 wn = ctk.CTk()
@@ -31,17 +44,21 @@ wn.resizable(False, False)
 wn.geometry("1280x720")
 
 buttons = []
-for i in range(5):
+i = 0
+for (category, question_list) in questions.items():
     question_row = []
-    for j in range(5):
-        question = questions[j][i]
-        answer = answers[j][i]
+    j = 0
+    for item in question_list:
+        question = item["question"]
+        answer = item["answer"]
         question_row.append(
             Question(wn, answer=answer, question=question, row=i, column=j)
         )
+        j+=1
     wn.grid_columnconfigure(i, pad=GRID_PAD)
     wn.grid_rowconfigure(i, pad=GRID_PAD)
     buttons.append(question_row)
+    i += 1
 
 
 wn.mainloop()
